ed52f5c440c34362e4f674d1b60debe8681820fe
Mike Dowd <mikedowd1972@gmail.com>
2019-02-24 13:39:21 -0700

    Add default table conditions

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const deck = require('./models/deck.model');
+const defaultConditions = require('./utilities/default-conditions');
 
 app.use(bodyParser.json());
 app.use(cors({origin: "http://localhost:3000"}));
 
-app.get('/single-screen', (req, res) => res.json({ deck }));
+app.get('/single-screen', (req, res) => {
+  res.json({ deck, defaultConditions })
+});
 
 app.listen(8090, () => console.log('blackjack server is listening on 8090...'));
