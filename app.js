2285f0c78c1b2707cffd8568f27ac5709555296d
Mike Dowd <mikedowd1972@gmail.com>
2019-02-24 13:41:31 -0700

    Refactor

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const defaultConditions = require('./utilities/default-conditions');
 app.use(bodyParser.json());
 app.use(cors({origin: "http://localhost:3000"}));
 
-app.get('/single-screen', (req, res) => {
-  res.json({ deck, defaultConditions })
-});
+app.get('/single-screen', (req, res) => res.json({ deck, defaultConditions }));
 
 app.listen(8090, () => console.log('blackjack server is listening on 8090...'));
