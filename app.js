846f5969a7f617658c9c25ae70acaf6e70cc2a12
Mike Dowd <mikedowd1972@gmail.com>
2019-02-23 18:57:02 -0700

    Add deck model, return from main route

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const deck = require('./models/deck.model');
 
 app.use(bodyParser.json());
 app.use(cors({origin: "http://localhost:3000"}));
 
-app.get('/single-screen', (req, res) => res.json({table: {}}));
+app.get('/single-screen', (req, res) => res.json({ deck }));
 
 app.listen(8090, () => console.log('blackjack server is listening on 8090...'));
